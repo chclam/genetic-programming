b3fd2129ade79209a635968a4c7fb4823eb41f46
Chris Lam <chris@chrislam.nl>
2022-02-11 15:26:07 +0100

    working conventional genetic vc

---
+++ geneticvc.py
@@ -0,0 +1,76 @@
+import random
+import numpy as np
+from tqdm import tqdm
+
+class GeneticVC():
+  def __init__(self, verts, adj_li, k):
+    '''
+    Parameterized vertex cover using a genetic algorithm.
+    '''
+    self.verts = verts
+    self.adj_li = adj_li
+    self.k = k
+    self.pop = None
+
+  def run(self, n, m):
+    if self.pop is None:
+      pop = self.init_pop(m)
+    gen = 0
+    for r in tqdm(range(n)):
+      fitness = self.get_fitness(pop)
+      i = 0
+      while i < m:
+        new_pop = []
+        indiv = random.choices(pop, weights=fitness, k=1)[0]
+        p = random.random()
+        if p <= 0.4:
+          # reproduce
+          new_pop.append(indiv)
+        elif 0.4 < p and p <= 0.5:
+          new_pop.append(self.mutate(indiv))
+        else:
+          indiv, indivv = random.choices(pop, weights=fitness, k=2)
+          new_pop += self.crossover(indiv, indivv)
+          i += 1
+        i += 1
+      pop += new_pop
+    self.pop = pop
+    return self
+    
+  def get_fittest(self):
+    i = np.argmax(self.get_fitness(self.pop))
+    return self.pop[i]
+
+  def mutate(self, x):
+    i = random.randint(0, len(x)-1)
+    diff = list(set(self.verts) - set(x))
+    x[i] = random.choice(diff)
+    return x
+
+  def crossover(self, x, y):
+    assert(len(x) == len(y))
+    i = random.randint(0, len(x)-1)
+    l = x[:i]
+    r = y[i:]
+    return [l+r, r+l]
+
+  def get_fitness(self, pop):
+    # fitness based on edge coverage
+    tot_edges = sum([len(adj) for adj in self.adj_li.values()]) // 2
+    ret = []
+    for indv in pop:
+      ret.append(self.get_coverage(indv))
+    ret = [v / tot_edges for v in ret]
+    return ret
+
+  def init_pop(self, pop_size):
+    return [random.sample(range(0, len(self.verts)-1), self.k) for i in range(pop_size)]
+    
+  def get_coverage(self, verts):
+    edges = []
+    for p in verts:
+      for q in self.adj_li[p]:
+        if (q, p) not in edges:
+          edges.append((p, q))
+    return len(edges)
+
--- vertexcover.py
+++ vertexcover.py
@@ -1,39 +1,37 @@
-from geneticprogram import GeneticVertexCover
+from geneticvc import GeneticVC
 
 def correct_adj_li(adj_li):
   for k in adj_li.keys():
-    # make symmetric
-    adj_k = []
+
+    adj_li[k] = [v for v in adj_li[k] if v != k]
+
     for v in adj_li[k]:
       if k not in adj_li[v]:
         adj_li[v].append(k)
-      if k != v:
-        adj_k.append(v)
-    adj_li[k] = adj_k
 
-  # remove duplicates and sort
   for k in adj_li.keys():
     adj_li[k] = sorted(list(set(adj_li[k])))
   
 if __name__ == "__main__":
   k = 5
-  verts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  verts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   adj_li = {
-    1: [2, 3, 6],
-    2: [10, 3, 1, 7, 4],
-    3: [6, 3, 1, 8],
-    4: [9, 10, 6],
-    5: [8, 2, 6, 1],
-    6: [9, 1, 3],
-    7: [6],
-    8: [2],
-    9: [8],
-    10: [5]
+    0: [2, 3, 6],
+    1: [9, 3, 1, 7, 4],
+    2: [6, 3, 1, 8],
+    3: [9, 1, 6],
+    4: [8, 2, 6, 1],
+    5: [9, 1, 3],
+    6: [6],
+    7: [2],
+    8: [8],
+    9: [5]
   }
+
   correct_adj_li(adj_li)
 
-  gen_vert = GeneticVertexCover(k)
-  gen_vert.fit(verts, adj_li)
-  gen_vert.run(n=10, gen_size=20)
-  print(gen_vert.pop)
+  gen_vert = GeneticVC(verts, adj_li, k)
+  gen_vert.run(n=10, m=20)
+  fittest = gen_vert.get_fittest()
+  print(fittest)
 
